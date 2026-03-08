ea6cc47c571f1b6ada4fb15c5d3bf7a88905d37f
chenglan <71644112+chenglan29@users.noreply.github.com>
2026-03-08 13:19:44 +0800

    Delete station-1.14.14 directory

--- station-1.14.14/chunk/route/getUserList.js
+++
@@ -1,7 +0,0 @@
-app.get('/getUserList', (req, res) => {
-    var user = db("user");
-    res.status(200).json({
-        success: true,
-        userList: Object.keys(user)
-    });
-});
--- station-1.14.14/db.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-module.exports = (name) => {
-    return JSON.parse(fs.readFileSync(`db/${name}.json`).toString());
-}
